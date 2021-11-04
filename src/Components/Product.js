82e9473a6752f66f3e2ef18e2e14809590aca119
hamzashadeez <hahmad11178@gmail.com>
2021-11-04 03:16:03 -0700

    Whatsapp share function

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -2,9 +2,13 @@ import React from "react";
 import "./product.css";
 import { useRecoilState } from "recoil";
 import { ImagePreview } from "../recoil/Image";
+import {Link} from 'react-router-dom'
 
 function Product({ data, id, showIt }) {
   const [image, setImage] = useRecoilState(ImagePreview);
+  const link = data?.photo;
+  const encoded = link.replace("&", "%26")
+  const encoded2 = encoded.replace("images/", "%26")
   return (
     <div
       className="-pb-1 shadow-sm bg-white product_item"
@@ -13,12 +17,20 @@ function Product({ data, id, showIt }) {
       <div className="mt-3 px-3">
         <h6 className="mb-0">{data?.name}</h6>
         <div className="mb-0 d-flex mt-2 justify-content-between">
-          <h5 className="mb-0 text-success">NGN{data?.price}</h5>
+          <h5 className="mb-0 text-success">NGN{" "}{data?.price}</h5>
         </div>
       </div>
-      <button className='btn btn-light border-1 w-100 mb-2 text-muted' onClick={()=>{
-        setImage(data?.photo);
-        showIt()}}>View</button>
+      <div className='d-flex mb-2 px-2 mt-1'>
+        <button className='btn btn-light border-1 w-100  text-muted' onClick={()=>{
+          setImage(data?.photo);
+          showIt()}}>View
+          </button>
+          <a href={"https://wa.me/2348032542898?text=Hi Idris,%20I%20need%20this:%20"+window.encodeURIComponent(data?.photo)}
+           target="_blank" className='btn btn-success px-4 w-100 ml-4'>
+             <i className='fa fa-whatsapp'></i>
+            </a>
+      </div>
+
     </div>
   );
 }
